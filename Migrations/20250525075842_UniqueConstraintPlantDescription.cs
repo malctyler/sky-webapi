f0d2b46436cd3376343ea1581b0c43a864cbfc22
Malcolm_Tyler <malcolm@malcsdomain.co.uk>
2025-05-25 10:12:15 +0100

    Add unique index on PlantDescription in AllPlant table

---
+++ Migrations/20250525075842_UniqueConstraintPlantDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sky_webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueConstraintPlantDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AllPlant_PlantDescription",
+                table: "AllPlant",
+                column: "PlantDescription",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AllPlant_PlantDescription",
+                table: "AllPlant");
+        }
+    }
+}
