c563d6c751240615a3dac311dc3da83e4daa5655
Malcolm Tyler <malcolm@malcsdomain.co.uk>
2025-06-07 20:01:14 +0100

    Add migration for LedgerReferenceWithoutInitials column

---
+++ Migrations/20250607190026_AddLedgerReferenceWithoutInitialsColumn_20250607.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sky_webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLedgerReferenceWithoutInitialsColumn_20250607 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
