23090c2d775a2ae09c64b40872a19e3fb1e5e23c
Malcolm Tyler <malcolm@malcsdomain.co.uk>
2025-06-20 00:01:32 +0100

    Add comprehensive Set-Cookie header debugging to AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -237,9 +237,15 @@ namespace sky_webapi.Controllers
                     tokenExpiration.ToString("O"), cookieOptions.Expires?.ToString("O"));                // Set cookie (works for same-domain, fallback for cross-domain)
                 Response.Cookies.Append("auth_token", generatedToken, cookieOptions);
                 
-                // Debug: Let's see what the browser should receive
-                var cookieValue = $"auth_token={generatedToken}; expires={cookieOptions.Expires:r}; path=/; secure; samesite=none";
-                _logger.LogInformation("DEBUG - Full cookie header should be: {CookieHeader}", cookieValue);var response = new AuthResponseDto
+                // Debug: Log the actual Set-Cookie header that will be sent to the browser
+                var cookieHeaderValue = $"auth_token={generatedToken}; expires={cookieOptions.Expires?.ToString("R")}; path={cookieOptions.Path}; secure; samesite=none";
+                _logger.LogInformation("DEBUG - Set-Cookie header value: {SetCookieHeader}", cookieHeaderValue);
+                  // Also log if there are any existing Set-Cookie headers
+                if (Response.Headers.ContainsKey("Set-Cookie"))
+                {
+                    var existingCookies = Response.Headers["Set-Cookie"];
+                    _logger.LogInformation("DEBUG - Existing Set-Cookie headers: {ExistingCookies}", string.Join("; ", existingCookies.ToArray()));
+                }var response = new AuthResponseDto
                 {
                     Id = user.Id,
                     Email = user.Email ?? string.Empty,
